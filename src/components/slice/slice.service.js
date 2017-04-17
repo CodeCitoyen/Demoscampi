bbd787cbc39824f5377ab75a2cd0a4d925cd2bd0
Pascal Chambon <chambon.pascal@gmail.com>
2017-04-17 19:33:12 +0200

    Increase back the timing of dialogs.

--- src/components/slice/slice.service.js
+++ src/components/slice/slice.service.js
@@ -24,11 +24,11 @@ function SliceService(I18n) {
         // No reading time for the user's slices
         return 0;
       }
-      // DEBUG *10
+      // NOTE - here lower the times, if you need to debug the game quickly
       // We read approximativly 200 words per minute
-      const duration = (this.text || '').split(' ').length * 60 / 200 * 100;
+      const duration = (this.text || '').split(' ').length * 60 / 200 * 1000;
       // Reading time can't be under 700 milliseconds
-      return Math.max(duration, 70);
+      return Math.max(duration, 700);
     }
     get type() {
       if (!this.meta.character) {
